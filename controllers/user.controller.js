4a152f3e23fc169bb006cb0166b59bad76f11fa4
Rahul M <roboto7o32oo3@gmail.com>
2023-09-23 00:46:40 +0530

    added controllers for getting projects under user

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,5 +1,28 @@
 const Project = require("../models/project.model")
 
+const getUserProjects = async (req, res, next) => {
+  const userId = req.params.userId;
+
+  let projects;
+
+  try {
+    projects = await Project.find({ userId });
+  }
+  catch (err) {
+    const error = new Error('Something went wrong in the server inside getUserProjects');
+    res.status(500);
+    return next(error);
+  }
+
+  if (!projects || projects.length === 0) {
+    const error = new Error('Could not find any projects under the user');
+    res.status(404);
+    return next(error);
+  }
+
+  res.json(projects.map(project => project.toObject({ getters: true })))
+}
+
 const getProjectByUserIdAndProjectName = async (req, res, next) => {
   const userId = req.params.userId;
   const projectName = req.params.projectName;
@@ -25,3 +48,4 @@ const getProjectByUserIdAndProjectName = async (req, res, next) => {
 }
 
 exports.getProjectByUserIdAndProjectName = getProjectByUserIdAndProjectName;
+exports.getUsersProjects = getUserProjects;
--- routes/user.route.js
+++ routes/user.route.js
@@ -1,8 +1,9 @@
 const express = require("express")
 const router = express.Router()
 
-const { getProjectByUserIdAndProjectName } = require("../controllers/user.controller")
+const { getProjectByUserIdAndProjectName, getUsersProjects } = require("../controllers/user.controller")
 
+router.get('/:userId/projects', getUsersProjects);
 router.get('/:userId/projects/:projectName', getProjectByUserIdAndProjectName);
 
 module.exports = router;
